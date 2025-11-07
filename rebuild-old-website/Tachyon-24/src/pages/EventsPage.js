456b0a0b6382c9fdb9d310d44b6ca4d18a016fc6
Ayush <prakashayush414@gmail.com>
2025-11-07 22:28:45 +0530

    Eveent updated

--- rebuild-old-website/Tachyon-24/src/pages/EventsPage.js
+++ rebuild-old-website/Tachyon-24/src/pages/EventsPage.js
@@ -1,84 +1,86 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './EventsPage.css';
-import { MapPin ,Clock4} from 'lucide-react';
+import { MapPin, Clock4 } from 'lucide-react';
 import Footer from "../components/Footer";
 
 const eventsData = { 
   "CSE": {
     "Rospinot": [
-      { id: 1, name: "Robo Bazzinga", description: "Rospinot fun maze solving challenge for robots", venue: "AB Corridor", time: "17th, 10:00 AM" },
-      { id: 2, name: "Cicada 036", description: "A mysterious online puzzle, with complex cryptography and intelligence clues", venue: "LT-2", time: "16th, 2:00 PM - 6:00 PM"},
-      { id: 3, name: "Robo race", description: "Robots compete to score goals on the field", venue: "AB Corridor", time: "16th, 6:00 PM - 8:00 PM" },
-      ,
+      { id: 1, name: "Robo Race", description: "Witness two robots clash in a thrilling soccer showdown — a test of precision, control, and strategy!", venue: "AB Corridor", time: "Day 3: 10:00 AM - 1:00 PM" },
+      { id: 2, name: "Cicada 036", description: "Epiphany is upon you. The pilgrimage begins — chase enlightenment, if you dare.", venue: "LT-2", time: "Day 2: 2:00 PM - 6:00 PM" },
+      { id: 3, name: "Robo Race F1", description: "Feel the rush as Lego F1-style robots blaze down the track, racing head-to-head for ultimate glory!", venue: "AB Corridor", time: "Day 3: 3:00 PM - 6:00 PM" },
     ],
     "CSI-Bitwise": [
-      { id: 5, name: "Newbie Code", description: "Kodeathon for freshman year", venue: "CL-2, CL-3, CL-4, CL-5" },
-      { id: 6, name: "Blind Code", description: "Test your coordination in a challenging blindfolded coding relay", venue: "CL-3, CL-4" },
-      { id: 7, name: "Code Mania", description: "Multiple round coding event", venue: "Online" },
-      { id: 8, name: "Bug Hunt", description: "Find and fix bugs in the code", venue: "CL-3, CL-4" },
-      { id: 9, name: "Debug Meme", description: "Debugging with a twist of humor", venue: "Online" },
+      { id: 5, name: "Newbie Code", description: "Kodeathon for freshman year", venue: "CL (ALL)", time: "Day 3: 11:00 AM - 1:00 PM" },
+      { id: 6, name: "Blind Code", description: "Test your coordination in a challenging blindfolded coding relay", venue: "CL-3 & CL-5", time: "Day 2: 1:30 PM - 3:30 PM" },
+      { id: 7, name: "Code Mania", description: "Multiple round coding event", venue: "Online", time: "Day 1: 7:00 PM - 10:00 PM" },
+      { id: 9, name: "Debug Meme", description: "Only the smartest minds get the joke", venue: "Online", time: "Day 3: 4:00 PM - 6:00 PM" },
     ],
     "CSI-Development": [
-      { id: 10, name: "Snake And CSS", description: "Creative CSS challenge", venue: "LT-2" },
-      { id: 11, name: "HackSprint", description: "Code against the clock to solve real-world problems (Hackathon)", venue: "Tagore Hall" },
-      { id: 12, name: "Smash Cart", description: "Gaming event", venue: "LT-2" },
-      { id: 13, name: "Build It Wrong", description: "Anti-coding challenge", venue: "CL-4" },
-      { id: 14, name: "Vibe Coding Hackathon", description: "2–3 hour hackathon where participants solve problem statements using vibe coding tools. Focus on creativity, style, and fun with animations, themes, and playful designs", venue: "CL-4" },
+      { id: 10, name: "Snake And CSS", description: "Control the snake's movement using CSS — move it with justify-content and align-items", venue: "LT-2", time: "Day 3: 2:00 PM - 4:00 PM" },
+      { id: 11, name: "HackSprint", description: "Code against the clock to solve real-world problems (Hackathon)", venue: "MPH", time: "Day 2: 8:00 PM - 11:00 PM" },
+      { id: 12, name: "Smash Cart", description: "Gaming event", venue: "LT-2", time: "Day 1: 7:00 PM - 8:00 PM" },
+      { id: 13, name: "Build It Wrong", description: "Collaborate seamlessly to tackle coding challenges in relay style", venue: "LT-1", time: "Day 3: 4:00 PM - 6:00 PM" },
+      { id: 14, name: "Vibe Coding Hackathon", description: "A fast-paced hackathon where vibe coders blend logic, creativity, and style to craft fun, animated, and expressive projects!", venue: "CL-4", time: "Day 2: 1:00 PM - 3:00 PM" },
     ],
     "CSI-Botnet": [
-      { id: 15, name: "Cryptic Hunt", description: "Decode and debug your way through a cryptic, beginner-friendly hunt", venue: "CL-4" },
-      { id: 16, name: "Mine Arena", description: "Minecraft technical challenge", venue: "Open ground near Nehru Plaza" },
-      { id: 17, name: "LAN Party", description: "Multiplayer gaming event", venue: "CL-2" },
+      { id: 15, name: "Cryptic Hunt", description: "Solve thrilling digital riddles and race to uncover hidden clues", venue: "LT-1", time: "Day 1: 7:00 PM - 8:00 PM" },
+      { id: 16, name: "Mine Arena", description: "Build, fight, and survive in the ultimate Minecraft competition", venue: "Nehru Plaza", time: "Day 3: 11:00 AM - 3:00 PM" },
+      { id: 17, name: "LAN Party", description: "Battle it out in an intense local gaming showdown with your squad", venue: "CL-4", time: "Day 2: 3:00 PM - 5:00 PM" },
     ],
     "CSI-MM&UI/UX": [
-      { id: 18, name: "GLITCH 1", description: "E-sports zone", venue: "LT-6 LT-7" },
-      { id: 19, name: "Arcade Box", description: "A fun retro game base console system for reliving the old games era", venue: "Nehru Plaza" },
-      { id: 20, name: "Typoolymics", description: "Speed typing event", venue: "CL-3" },
-      { id: 21, name: "GLITCH 2", description: "E-sports zone", venue: "LT-5" },
+      { id: 18, name: "GLITCH 1", description: "BGMI and RC tournament", venue: "LT-6 & LT-7", time: "Day 2: 2:00 PM - 6:00 PM" },
+      { id: 19, name: "Arcade Box", description: "A fun retro game base console system for reliving the old games era", venue: "Nehru Plaza", time: "Day 2: 1:00 PM - 4:00 PM" },
+      { id: 20, name: "Typolymics", description: "Speed typing event", venue: "CL-3", time: "Day 2: 4:00 PM - 5:00 PM" },
+      { id: 21, name: "GLITCH 2", description: "BGMI and RC tournament", venue: "LT-5", time: "Day 3: 12:00 PM - 3:00 PM" },
+      { id: 22, name: "Memeathon", description: "Event based on creating and designing memes", venue: "Online", time: "Day 1: All Day" },
     ],
     "Mozilla": [
-      { id: 22, name: "Website Pixel Art", description: "Fun event where students have to find correct answers for problems", venue: "Online" },
-      { id: 23, name: "War For Trezor", description: "Web security challenge", venue: "Online" },
-      { id: 24, name: "Hack in a Box", description: "Online coding challenge for beginners with a virtual 'mystery box' of prompts, APIs, and fun twists", venue: "Online" }
+      { id: 23, name: "Pixel Art", description: "Create stunning digital artworks using vibrant pixel-style graphics", venue: "Online", time: "Day 1: All Day" },
+      { id: 24, name: "War For Trezor", description: "War for Trezor — an exciting quiz battle to unlock hidden treasures", venue: "Online", time: "Day 2: All Day" },
+      { id: 25, name: "Vibe Coding", description: "Vibe Code — Showcase your creativity by building an impressive website or game with standout UI/UX design", venue: "Online", time: "Day 3: All Day" }
+    ],
+    "VR AR MR": [
+      { id: 26, name: "2D Gaming", description: "A structured session dedicated to playing and enjoying two-dimensional video games via a dedicated booth", venue: "AB Corridor", time: "Day 1: 7:00 PM - 9:00 PM" },
+      { id: 27, name: "Dare Tiles", description: "An interactive dare game likely involving participants performing challenges determined by tiles", venue: "Nehru Plaza", time: "Day 2: 3:00 PM - 7:00 PM" },
+      { id: 28, name: "Haunted House", description: "An immersive attraction featuring a Virtual Reality (VR) haunted house experience", venue: "AB Corridor", time: "Day 3: 5:00 PM - 7:00 PM" },
+    ],
+    "GFG": [
+      { id: 29, name: "Add-O-Create", description: "Creative blender design event", venue: "AB Corridor", time: "Day 2: 2:00 PM - 4:00 PM" },
+      { id: 30, name: "Stumble Guyz", description: "A fun gaming event with obstacles and competition", venue: "LT-1", time: "Day 2: 4:00 PM - 6:00 PM" },
+      { id: 31, name: "QuizSprint", description: "Fast-paced buzzer quiz", venue: "Online", time: "Day 3: 11:00 AM - 12:30 PM" },
     ]
   },
   "ISF": [
-    { id: 30, name: "Robo War", description: "Robot combat challenge", venue: "Arena", image: "./eventgame5.jpeg", time: "16th, 6:00 - 8:00 PM" },
-    { id: 31, name: "Laser Tag", description: "Event in dark where you have to find the right path for your laser", venue: "In front of ISF Room", image: "./eventgame1.jpeg", time: "15th, 7:00 - 9:00 PM" },
-    { id: 32, name: "Robo Race", description: "A high-speed race where robots navigate challenging tracks to the finish line", venue: "Chhota Mandir", time: "17th, 10:00 AM - 12:00 PM" },
-    { id: 33, name: "Maze Bot", description: "Navigate unseen twists in the ultimate maze challenge", venue: "In front of Tuck 1", time: "17th, 2:00 - 4:00 PM" },
-    // { id: 34, name: "Pinball", description: "Robotics challenge", venue: "Arena", time: "16th, 3:00 - 5:00 PM" },
-    { id: 34, name: "Junkyard War", description: "Treasure hunt to create something unique by finding hidden tools", venue: "In front of ISF room", time: "16th, 3:00 - 5:00 PM" },
-    // { id: 36, name: "Exhibition", description: "Robotics challenge", venue: "Arena", time: "16th, 6:00 - 8:00 PM" },
+    { id: 32, name: "Laser Tag", description: "Event in dark where you have to find the right path for your laser", venue: "In front of ISF Room", time: "Day 1: 6:00 PM - 9:00 PM" },
+    { id: 33, name: "Escape Room", description: "Solve fun computer and logic puzzles to find clues and escape the room before time runs out!", venue: "In front of ISF room", time: "Day 2: 1:00 PM - 3:00 PM" },
+    { id: 34, name: "Robo Race", description: "High-speed race where robots navigate challenging tracks", venue: "In front of Tuck-1", time: "Day 2: 4:00 PM - 7:00 PM" },
+    { id: 35, name: "Robo War", description: "Robots will compete against each other in an arena", venue: "Tuck-1", time: "Day 3: 4:00 PM - 6:00 PM" },
+    { id: 36, name: "TDM", description: "Gear up, take aim, and dominate the battlefield — only the fiercest survive in TDM!", venue: "In front of Tuck 1", time: "Day 3: 6:00 PM - 9:00 PM" },
   ],
   "MES": [
-    { id: 35, name: "Hawkeye Bow", description: "Mechanical bow design", venue: "Workshop", image: "./eventgame2.jpeg", time: "16th, 5:00 - 7:00 PM" },
-    { id: 36, name: "Buggy Ride", description: "A funny buggy ride demo", venue: "MES Club", image: "./eventgame3.jpeg", time: "17th, 5:00 - 7:00 PM" },
-    { id: 37, name: "Assembling Gear", description: "A speed test competition for assembling and de-assembling of gearbox", venue: "MES Club", time: "16th, 6:00 - 7:30 PM" },
-    { id: 38, name: "Waterjet", description: "Mechanical bow design", venue: "Workshop", time: "16th, 3:00 - 5:00 PM" },
-    { id: 39, name: "Spring Suspension Car", description: "Event in which basic principles of elasticity will be used to make cars", venue: "MES Club", time: "15th, 8:00 - 9:00 PM" },
-    { id: 40, name: "DIY Engineering Competition", description: "Showcasing your coordination and engineering skills", venue: "MES Club", time: "17th, 7:00 - 8:00 PM" },
-    { id: 41, name: "WorkShop Worriror", description: "Design something creative by using carpentary skills", venue: "MES Department", time: "17th, 11:00 AM - 1:00 PM" },
+    { id: 37, name: "Spring Suspension Car", description: "Event in which basic principles of elasticity will be used to make cars", venue: "MES Club", time: "Day 1: 8:00 PM - 9:00 PM" },
+    { id: 38, name: "Equilibria", description: "Two participants have to write the given word using pen tied with thread", venue: "Nehru Plaza", time: "Day 3: 11:00 AM - 1:00 PM" },
+    { id: 39, name: "Hawkeye Bow", description: "Archery sport", venue: "MES Club", time: "Day 2: 5:00 PM - 7:00 PM" },
+    { id: 40, name: "Buggy Ride", description: "One participant has to pick the ball from one box to another in limited time", venue: "Nehru Plaza", time: "Day 3: 5:00 PM - 7:00 PM" },
+    { id: 41, name: "Gear Assembling", description: "Showcasing your coordination and engineering skills", venue: "MES Club", time: "Day 3: 7:00 PM - 8:00 PM" },
   ],
   "CE": [
-    { id: 42, name: "Truss It", description: "Design and build strong truss structures to test load-bearing and structural efficiency", venue: "AB Corridor", image: "./eventgame4.jpeg" , time: "15th, 7:00 - 9:00 PM"},
-    { id: 43, name: "TIC TAC TOE", description: "Strategic building game", venue: "Lab", image: "./eventgame5.jpeg", time: "17th, 5:00 - 6:30 PM" },
-    { id: 44, name: "Float The Ball", description: "A team event to create a seamless pathway for a ball using coordinated pipe movements.", venue: "Nehru Plaza", time: "17th, 2:00 - 3:00 PM" },
-    { id: 45, name: "Clay Modelling", description: "Craft and shape unique designs with clay this artistic challenge", venue: "CE Dept" , time: "17th, 10:00 - 11:30 AM"},
-    { id: 46, name: "DEFYROULETTE", description: "Scan Virtual card in AR to complete thrilling real-world dare", venue: "Nehru Plaza" , time: "16th, 6:00 - 7:00 PM"},
+    { id: 42, name: "Truss It", description: "Design and build strong truss structures to test load-bearing and structural efficiency", venue: "AB Corridor", time: "Day 1: 7:00 PM - 9:00 PM" },
+    { id: 43, name: "Clay Modelling", description: "Craft and shape unique designs with clay this artistic challenge", venue: "CE Dept", time: "Day 3: 12:00 PM - 2:00 PM" },
+    { id: 44, name: "Float The Ball", description: "A team event to create a seamless pathway for a ball using coordinated pipe movements", venue: "Nehru Plaza", time: "Day 3: 2:00 PM - 3:00 PM" },
+    { id: 45, name: "Tic Tac Toe", description: "Strategic building game", venue: "Near Cafeteria", time: "Day 3: 5:00 PM - 6:30 PM" },
   ],
   "CHE": [
-    { id: 49, name: "Flame Fun", description: "Colourful flames from different salts", venue: "AB Corridor", image: "./eventgame1.jpeg", time: "17th, 10:00 - 11:00 AM" },
-    { id: 50, name: "Litmus Art", description: "Using the properties of turmeric powder, letting people draw on plain canvas", venue: "AB Corridor", image: "./eventgame2.jpeg" , time: "16th, 1:00 - 3:00 PM"},
-    { id: 51, name: "Non-Newtonian Fluid", description: "Your force matters", venue: "Lab", time: "17th, 1:00 - 2:00 PM" },
-    { id: 52, name: "Chemical Chameleon", description: "Color change automation by titration", venue: "Lab", time: "17th, 5:00 - 6:00 PM" },
-    { id: 53, name: "Lava Lamp", description: "Miniature volcano by different chemicals", venue: "Lab", time: "16th, 1:00 - 3:00 PM" },
-    { id: 54, name: "Racing Car", description: "Fun event in which chemical compounds will be used as fuel for racing cars", venue: "In front of Tuck-2", time: "16th, 1:00 - 2:00 PM" },
+    { id: 46, name: "Go Slow", description: "Chemical engineering challenge", venue: "AB Corridor", time: "Day 2: 3:00 PM - 5:00 PM" },
+    { id: 47, name: "Locked Up", description: "Chemical puzzle challenge", venue: "AB Corridor", time: "Day 3: 2:00 PM - 4:00 PM" },
+    { id: 48, name: "Detective Byomkesh", description: "Solve mysteries using chemical clues", venue: "AB Corridor", time: "Day 3: 2:00 PM - 4:00 PM" },
   ],
-  "IIC":[
-    {id:55,name:"Bit to Build" ,description:"Founders Bidding Leaque",venue:"LT-1", image: "./eventgame2.jpeg",time:"17th,11am"},
-    
+  "EDC": [
+    { id: 49, name: "InnovateX", description: "The Ultimate Idea Pitching Challenge", venue: "LT-8", time: "Day 2: 3:00 PM - 5:00 PM" },
+    { id: 50, name: "Tech Sprint", description: "Scavenger hunt designed to challenge your tech knowledge", venue: "AB Corridor", time: "Day 3: 11:00 AM - 2:00 PM" },
+    { id: 51, name: "Gameathon", description: "Gaming competition", venue: "Online", time: "Day 2: All Day" },
   ]
 };
 
@@ -88,7 +90,7 @@ const branchImages = {
   'MES': '/event_main_logo/test/three.png',
   'CE': '/event_main_logo/test/four.png',
   'CHE': '/event_main_logo/test/ele.png',
-  'IIC': '/event_main_logo/test/five.png'
+  'EDC': '/event_main_logo/test/five.png'
 };
 
 const clubIcons = {
@@ -113,19 +115,19 @@ const clubImages = {
   'Mozilla': '/ultimate/mozilla_02.jpg'
 };
 
- const branchIcons = {
-   'CSE': '💻',
-   'ISF': '⚡',
-   'MES': '⚙️',
-   'CE': '🏗️',
-   'CHE': '🧪',
-   'IIC': '🚀'
+const branchIcons = {
+  'CSE': '💻',
+  'ISF': '⚡',
+  'MES': '⚙️',
+  'CE': '🏗️',
+  'CHE': '🧪',
+  'EDC': '🚀'
 };
 
 const extendedDescriptions = { 
-  1: "Test your skills in communicating ideas without words in this unique Robot Charades event, where robots mimic actions for participants to guess.",
-  2: "A fast-paced Robo Soccer competition where bots collide on the field for victory. Navigate, tackle, and score in a challenging arena.",
-  // More extended descriptions...
+  1: "Witness two robots clash in a thrilling soccer showdown — a test of precision, control, and strategy!",
+  2: "Epiphany is upon you. The pilgrimage begins — chase enlightenment, if you dare.",
+  3: "Feel the rush as Lego F1-style robots blaze down the track, racing head-to-head for ultimate glory!",
 };
 
 const Events = () => {
@@ -252,8 +254,8 @@ const Events = () => {
         <p>{event.venue}</p>
       </div>
       <div className="card-back">
-      <h3>{event.name}</h3>
-      <p className='event-p'>{event.venue}</p>
+        <h3>{event.name}</h3>
+        <p className='event-p'>{event.venue}</p>
         <p><strong>Description:</strong>{extendedDescriptions[event.id] || event.description}</p>
       </div>
     </div>
@@ -281,10 +283,8 @@ const Events = () => {
       </div>
     </div>
   );
-  
 
   const renderEvents = (events) => {
-    // Get the display name for the events
     const displayName = selectedClub || selectedBranch;
 
     return (
@@ -311,26 +311,26 @@ const Events = () => {
                 onClick={() => handleClubClick(club)}
                 data-club={club}
               >
-              <div className="club-image-container">
-                <img 
-                  src={clubImages[club]} 
-                  alt={club} 
-                  className="club-image"
-                  onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.style.display = 'none';
-                    e.target.nextSibling.style.display = 'block';
-                  }}
-                />
-                <div className="club-icon" style={{ display: 'none' }}>
-                  {clubIcons[club] || '🏛️'}
+                <div className="club-image-container">
+                  <img 
+                    src={clubImages[club]} 
+                    alt={club} 
+                    className="club-image"
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.style.display = 'none';
+                      e.target.nextSibling.style.display = 'block';
+                    }}
+                  />
+                  <div className="club-icon" style={{ display: 'none' }}>
+                    {clubIcons[club] || '🏛️'}
+                  </div>
                 </div>
+                <h3>{club}</h3>
+                <p>{branchData[club].length} Events</p>
               </div>
-              <h3>{club}</h3>
-              <p>{branchData[club].length} Events</p>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
         </>
       );
     }
